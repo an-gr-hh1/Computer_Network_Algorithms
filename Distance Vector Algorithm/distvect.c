38aa73a766e5e7113f71ef1fd456a3004fb13ec8
Anugrah Premachandran <158727858+an-gr-hh1@users.noreply.github.com>
2024-07-06 21:56:23 +0530

    Update distvect.c

--- Distance Vector Algorithm/distvect.c
+++ Distance Vector Algorithm/distvect.c
@@ -13,35 +13,35 @@ int main()
 	scanf("%d",&n);
 	printf("\nEnter the cost matrix :\n");
 	for(i=0;i<n;i++)
-    {
+    	{
 		for(j=0;j<n;j++ )
-        {
+        	{
 			scanf("%d",&dmat[i][j]);
 			dmat[i][i]=0;
 			rt[i].dist[j]=dmat[i][j];
 			rt[i].from[j]=j;  
-        }
-    }
+        	}
+    	}
 	do
-    {  
-        count=0;
-        for(i=0;i<n;i++)
-            for(j=0;j<n;j++)
-                for(k=0;k<n;k++)
-                if(rt[i].dist[j]>dmat[i][k]+rt[k].dist[j])
-                {
-                    rt[i].dist[j]=rt[i].dist[k]+rt[k].dist[j];
-                    rt[i].from[j]=k;
-                    count++;  
-                }
+    	{  
+	        count=0;
+	        for(i=0;i<n;i++)
+	            for(j=0;j<n;j++)
+	                for(k=0;k<n;k++)
+	                if(rt[i].dist[j]>dmat[i][k]+rt[k].dist[j])
+	                {
+	                    rt[i].dist[j]=rt[i].dist[k]+rt[k].dist[j];
+	                    rt[i].from[j]=k;
+	                    count++;  
+	                }
 	}while(count!=0);
-    for(i=0;i<n;i++)
-    {
-        printf("\n\nRouting Table for router %d : \n",i+1);
-        for(j=0;j<n;j++)
-        {
-            printf("\t\n Node %d via %d Distance:\t%d",j+1,rt[i].from[j]+1,rt[i].dist[j]);
-        }
-    }
+    	for(i=0;i<n;i++)
+	{
+		printf("\n\nRouting Table for router %d : \n",i+1);
+	        for(j=0;j<n;j++)
+	        {
+	            printf("\t\n Node %d via %d Distance:\t%d",j+1,rt[i].from[j]+1,rt[i].dist[j]);
+	        }
+	}
 	printf("\n\n");
 }
